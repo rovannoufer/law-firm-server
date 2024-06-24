29094602a36b86a74777bd373bebb4220b910968
rovannoufer <noufermohamed007@gmail.com>
2024-06-24 17:30:12 +0530

    corrections

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const uri = process.env.DB_LOCATION;
 
@@ -16,10 +18,26 @@ mongoose.connect(uri)
   .catch(err => console.log('Error connecting to MongoDB:', err));
 
 const schema = new mongoose.Schema({
-  section: Number,
+  section: String,
   title: String,
   description: String,
   id: Number,
+}); 
+
+app.get('/search', async (req, res) => {
+  const { section, collectionName } = req.query; 
+
+   console.log(section)
+   const client = await mongoose.connection.getClient();
+    const db = client.db("law");
+    const collection = db.collection(`${collectionName}`);
+    const result = await collection.findOne({ section: section  });
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(404).json({ error: 'Section not found' });
+  }
 });
 
 app.get('/fetch/:collectionName', async (req, res) => {
